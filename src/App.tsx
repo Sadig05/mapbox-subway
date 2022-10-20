1b2f66331e1718331d550399d2706c34d355db57
sadig05 <Sadiq.letifli70@gmail.com>
2022-10-20 12:07:02 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,168 @@
+import { useState, useEffect, useRef, useMemo } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Supercluster, { PointFeature } from "supercluster";
+import mapboxgl, { LngLatBounds } from "mapbox-gl";
+import Map, { Source, Layer, Marker, MapRef, Popup } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { Box, Center } from '@chakra-ui/react'
+import data from "./db.json";
+import CustomMarker from "./CustomMarker";
+const token =
+  "pk.eyJ1IjoicXJ0cG91bmRjaGVlc2UiLCJhIjoiY2w2ZTZ5YmJ0MDc2OTNicDVvaHd3YjVvMCJ9.cdoOOFa_7KBZtXVPgxfWnA";
+
+export interface IData {
+  [x: string]: any;
+  name: string;
+  url: string;
+  street_address: string;
+  city: string;
+  state: string;
+  zip_code: number;
+  country: string;
+  phone_number_1: string;
+  phone_number_2: string;
+  fax_1: string;
+  fax_2: string;
+  email_1: string;
+  email_2: string;
+  website: string;
+  open_hours: string;
+  latitude: number;
+  longitude: number;
+  facebook: string;
+  twitter: string;
+  instagram: string;
+  pinterest: string;
+  youtube: string;
+  cluster_id?: number;
+}
+
+const temp: any = data;
+
+const geoData: PointFeature<IData>[] = temp.map((row: IData) =>{
+
+return {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [row.longitude, row.latitude],
+    },
+    properties: row,
+  };
+});
+
+const index = new Supercluster({
+  radius: 40,
+  maxZoom: 16,
+});
+
+index.load(geoData);
+
+function App() {
+
+  const mapRef = useRef<MapRef>(null);
+  const [bounds, setBounds] = useState<LngLatBounds>();
+  const [markers, setMarkers] = useState<any>([]);
+  const [count, setCount] = useState(0);
+  const [children, setChildren] = useState<any>([]);
+  const [zoom, setZoom] = useState<number>(3);
+  // const handleChange = (e: any) => {
+  //   setBounds(e.target.getBounds());
+  // };
+
+  useEffect((): any => {
+    // const zooms = Object.keys(bounds).map((item: any) => {
+    //   return [bounds[item]?.lng, bounds[item]?.lat];
+    // });
+
+    // console.log(
+    //   "cluster",
+    //   index.getClusters([zooms[0][0], zooms[0][1], zooms[1][0], zooms[1][1]], 2)
+    // );
+    // setMarkers(
+    //   index.getClusters([zooms[0][0], zooms[0][1], zooms[1][0], zooms[1][1]], 2)
+    // );
+
+
+
+    
+
+
+    // setMarkers(
+    //   index.getClusters([bounds.getWest(),bounds.getSouth(), bounds.getEast(), bounds.getNorth()], 2)
+    // )
+
+
+    // markers.map((item : any) => {
+    //   if("cluster" in item.properties){
+    //     console.log(index.getLeaves(item.properties.cluster_id, Infinity, 100))
+    //   }
+      
+    // })
+
+    // setCount(markers.length);
+
+  }, [bounds]);
+
+
+
+
+  const Cluster = useMemo(() => {
+      if (!bounds) return;
+      const clusters = index.getClusters([bounds?.getWest(), bounds?.getSouth(), bounds?.getEast(), bounds?.getNorth()], zoom)
+      return(
+        clusters.map((item: any) => {
+          { 
+            let leaves: any = [];
+            let pointCount = item.properties.point_count;
+            if("cluster" in item.properties){
+             leaves = index.getLeaves(item.id, Infinity)
+            }
+            return (
+                <CustomMarker  lon = {item.geometry.coordinates[0]} lat = {item.geometry.coordinates[1]} count= {pointCount} item = {item} leaves ={leaves}/>
+            );
+          }
+        })
+     
+      )
+  },[bounds, zoom])
+
+
+
+
+
+  return (
+    <Map
+      initialViewState={{
+        longitude: -100,
+        latitude: 40,
+        zoom: 3,
+      }}
+
+      onZoomEnd={(e) => {
+        setZoom(e.target.getZoom())
+        setBounds(e.target.getBounds())
+    }}
+    onDragEnd={(e) => {
+        setZoom(e.target.getZoom())
+        setBounds(e.target.getBounds())
+    }}
+    onLoad={(e) => {
+        setZoom(e.target.getZoom())
+        setBounds(e.target.getBounds())
+    }}
+
+
+      // onZoom={handleChange}
+      style={{ width: "100vw", height: "100vh" }}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      mapboxAccessToken={token}
+    >
+      {Cluster}
+    
+    </Map>
+  );
+}
+
+export default App;
---
+++ src/CustomMarker.tsx
@@ -0,0 +1,75 @@
+import React, { useMemo, useState } from "react";
+import { Marker, Popup } from "react-map-gl";
+import { Box, Center } from "@chakra-ui/react";
+import { PointFeature } from "supercluster";
+import { IData } from "./App";
+import { FaArrowLeft, FaArrowRight, FaMapMarkerAlt } from "react-icons/fa";
+interface IProps {
+  lon: number;
+  lat: number;
+  count: number;
+  item: any;
+  leaves: any;
+}
+
+const CustomMarker = ({ lon, lat, count, item, leaves }: IProps) => {
+  const [showPopup, setShowPopup] = useState<null | PointFeature<IData>>(null);
+  const [index, setIndex] = useState<number>(0);
+
+  const increment = () =>{
+
+    if(index <= leaves.length){
+      setIndex(index + 1)
+    }
+    if(index >= leaves.length - 1){
+      setIndex(0)
+    }
+  }
+
+
+  const decrement = () => {
+    if(index <= leaves.length){
+      setIndex(index - 1)
+    }
+
+    if(index <= 0){
+      setIndex(leaves.length -1)
+    }
+   
+  }
+  const child = useMemo(() => {
+    if(!count){
+      <FaMapMarkerAlt size ={30}  />
+    }
+    return (
+      <button className="btn-cluster"
+      onClick={() => setShowPopup(item)}
+    >
+      <Center height={30}>{count ? `${count} ` : <FaMapMarkerAlt   size={30}/>}</Center>
+    </button>
+    );
+  }, [lon, lat]);
+  return (
+    <Marker longitude={lon} latitude={lat}>
+      {child}
+      {showPopup && (
+           <Popup
+           longitude={item.geometry.coordinates[0]}
+           latitude={item.geometry.coordinates[1]}
+           anchor="bottom"
+           onOpen={() => setShowPopup(item)}
+           onClose={() => setShowPopup(null)}
+           closeOnClick={false}
+         >
+           <>{count ? <><FaArrowLeft size = {20} onClick = {decrement}/> <FaArrowRight size = {20} onClick ={increment} /> </> : <></> }</>
+           <div>{count? leaves[index]?.properties?.name : item.properties.name}</div>
+           <div>{count ? leaves[index]?.properties?.city : item.properties.city}</div>
+           <div>{count ?  `${index+1}/${leaves.length}` : item.properties.street_address}</div>
+         </Popup>
+        
+        )}
+    </Marker>
+  );
+};
+
+export default CustomMarker;
